e40b69fdce118fadefdc7d21de82b1da8e576975
Aseem Sane <aseem191@gmail.com>
2018-07-27 17:38:26 -0700

    Add files via upload
    Found out how to store and retrieve arrays from text files - through JSON.

--- imagetoarrtest.py
+++ imagetoarrtest.py
@@ -1,10 +1,10 @@
 import numpy as np
 from PIL import Image, ImageTk
 import glob
-
+import json
 import csv
 
-def getimagearr():
+def placeimagearr():
 	image_list = []
 	i = 0
 
@@ -15,45 +15,11 @@ def getimagearr():
 		print(image_list[i].shape)
 		i = i + 1
 
-	with open('imagetest.txt', 'w') as csvfile:
-			matrixwriter = csv.writer(csvfile, delimiter=' ')
-			for row in image_list:
-				matrixwriter.writerow(row)
+	with open("imagetest.txt", "w") as outfile:
+		json.dump(np.array(image_list).tolist(), outfile)
 
+def getimagearr():
+	with open("imagetest.txt", "r") as infile:
+		image_list = json.load(infile)
+		print(np.array(image_list).shape)
 
-# image_list = []
-# i = 0
-
-# for filename in glob.glob('testfolder/*'): 
-# 	im = Image.open(filename).resize((1024, 786), Image.ANTIALIAS).convert('L')
-# 	arr = np.array(im)
-# 	image_list.append(arr)
-# 	print(np.array(image_list).shape)
-# 	i = i + 1
-
-
-# with open('imagetest.txt', 'w') as csvfile:
-# 	i = 0
-# 	matrixwriter = csv.writer(csvfile, delimiter='\t')
-# 	for row in image_list:
-# 		matrixwriter.writerow(row)
-# 		i = i + 1
-# 		print(i)
-
-
-image_list = np.array([])
-real_list = np.array([])
-
-
-with open('imagetest.txt', 'r') as csvfile:
-	matrixreader = csv.reader(csvfile, delimiter='\t')
-	matrixreader = list(matrixreader)
-	i= 0
-	for row in matrixreader:
-		image_list = np.concatenate((image_list, np.array(row)))
-		i = i + 1
-		print(i)
-
-#print((image_list[0].replace("[", "").split())[0])
-
-print(image_list.shape)
