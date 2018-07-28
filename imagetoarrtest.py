6e348f38bc81965357b1151786782c96e536c3b0
Aseem Sane <aseem191@gmail.com>
2018-07-27 17:23:35 -0700

    Add files via upload
    Image to grayscale works, haven't found a reliable way to read/write arrays to files.

--- imagetoarrtest.py
+++ imagetoarrtest.py
@@ -12,22 +12,48 @@ def getimagearr():
 		im = Image.open(filename).resize((1024, 786), Image.ANTIALIAS).convert('L')
 		arr = np.array(im)
 		image_list.append(arr)
-		matrixwriter.writerow(row)
 		print(image_list[i].shape)
 		i = i + 1
 
-	with open('imagetest.txt', 'wb') as csvfile:
+	with open('imagetest.txt', 'w') as csvfile:
 			matrixwriter = csv.writer(csvfile, delimiter=' ')
 			for row in image_list:
 				matrixwriter.writerow(row)
 
 
-image_list = []
+# image_list = []
+# i = 0
+
+# for filename in glob.glob('testfolder/*'): 
+# 	im = Image.open(filename).resize((1024, 786), Image.ANTIALIAS).convert('L')
+# 	arr = np.array(im)
+# 	image_list.append(arr)
+# 	print(np.array(image_list).shape)
+# 	i = i + 1
+
+
+# with open('imagetest.txt', 'w') as csvfile:
+# 	i = 0
+# 	matrixwriter = csv.writer(csvfile, delimiter='\t')
+# 	for row in image_list:
+# 		matrixwriter.writerow(row)
+# 		i = i + 1
+# 		print(i)
+
+
+image_list = np.array([])
+real_list = np.array([])
 
 
 with open('imagetest.txt', 'r') as csvfile:
-	matrixreader = csv.reader(csvfile, delimiter=' ')
+	matrixreader = csv.reader(csvfile, delimiter='\t')
+	matrixreader = list(matrixreader)
+	i= 0
 	for row in matrixreader:
-		image_list.append(row)
+		image_list = np.concatenate((image_list, np.array(row)))
+		i = i + 1
+		print(i)
 
-print(np.array(image_list[0][0][0]))
+#print((image_list[0].replace("[", "").split())[0])
+
+print(image_list.shape)
